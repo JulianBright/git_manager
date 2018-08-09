7a32908ab6da8d7f505198e38bd6e7c4e24d59d3
JulianBright <38367981+JulianBright@users.noreply.github.com>
2018-08-09 18:42:53 +0800

    to create my TestGit.java.
    this is my first time to use git to manager my code.

---
+++ TestGit.java
@@ -0,0 +1,10 @@
+//use github to manage my code
+//it seems it's a must for managing code.
+public class TestGit
+{
+  public static void main(String[] args)
+  {
+    System.out.println("git to manage my code");
+    System.exit(0);
+  }
+}
